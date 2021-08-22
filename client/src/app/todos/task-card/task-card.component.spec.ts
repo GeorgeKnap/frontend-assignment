4753f03f2f80b3647013253661eef1cebe2d86ef
George Knap <knap.george@gmail.com>
2021-08-22 22:57:00 +0200

    test(task card): expansion panel exists test

--- client/src/app/todos/task-card/task-card.component.spec.ts
+++ client/src/app/todos/task-card/task-card.component.spec.ts
@@ -1,4 +1,7 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatExpansionPanelHarness } from '@angular/material/expansion/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@todos/shared';
 import { TaskCardComponent } from './task-card.component';
@@ -7,6 +10,7 @@ import { TaskCardComponent } from './task-card.component';
 describe('TaskCardComponent', () => {
   let component: TaskCardComponent;
   let fixture: ComponentFixture<TaskCardComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +22,7 @@ describe('TaskCardComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskCardComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     component.task = {
       id:1,
@@ -34,4 +39,11 @@ describe('TaskCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('expansion panel should exist', async () => {
+    const expansionPanel = await loader.getHarness(MatExpansionPanelHarness);
+    console.log(expansionPanel);
+    expect(expansionPanel).toBeTruthy();
+  });
+
 });
